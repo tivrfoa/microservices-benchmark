6a0b7884d0ecd18fb70c5be3e3b9feac9c142c15
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-10-27 19:21:20 -0300

    use await instead of then

--- oak2.ts
+++ oak2.ts
@@ -5,30 +5,14 @@ interface Person {
 	Age: number,
 }
 
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
-
 const router = new Router();
 router
   .get("/", (context) => {
     context.response.body = "Hello world!";
   })
-  .get("/books", (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .post("/personCreate", (context) => {
-	  console.log(context);
-	  console.log(context.request);
-	  let body = context.request.body();
-	  console.log(body);
-	  let value = body.value;
-	  console.log(value, typeof value);
-	  value.then(r => console.log("req: " + r));
-    context.response.body = "todo";
+  .post("/personCreate", async (context) => {
+	  let person = await context.request.body().value as Person;
+	  context.response.body = `${person.Name} is ${person.Age} years old`;
   });
 
 const app = new Application();
