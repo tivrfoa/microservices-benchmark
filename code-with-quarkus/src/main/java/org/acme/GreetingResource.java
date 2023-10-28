97fbfafa42d127bc090b5bad2154dcfe30300fcc
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-10-27 22:29:55 -0300

    add Java Quarkus

---
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,36 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import io.smallrye.common.annotation.NonBlocking;
+
+@Path("/")
+public class GreetingResource {
+
+	public static class Person {
+		public String Name;
+		public byte Age;
+		public String toString() {
+			return "{ Name: '" + Name + "', Age: " + Age + " }";
+		}
+	}
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Quarkus";
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+	@Path("personCreate")
+	// @NonBlocking
+    public String personCreate(Person person) {
+		System.out.println(person);
+        return person.Name + " is " + person.Age + " years old";
+    }
+}
